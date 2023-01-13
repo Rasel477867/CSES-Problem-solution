111876fdb76bfa5ff927728e153e3bf300e597aa
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-01-13 07:41:33 +0600

    Movie Festival Solution (Searching and sorting)
    https://cses.fi/problemset/result/5276790/

--- code.cpp
+++ code.cpp
@@ -5,36 +5,35 @@ using namespace std;
 #define pb push_back
 #define ps push
 #define pp pop
-map<ll,ll>m;
-map<ll,ll>:: iterator it;
+vector<pair<ll,ll> >v;
 
 
-
-
-vector<ll>v;
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll n,a,b,i;
+    ll n,a,b,c,i;
     cin>>n;
-    while(n--)
+    for(i=0; i<n; i++)
     {
         cin>>a>>b;
-        m[a]+=1;
-        m[b+1]+=-1;
+        v.pb({b,a});
     }
-    for(it=m.begin(); it!=m.end(); it++)
-    {
-        v.pb(it->second);
-    }
-
-    for(i=1; i<v.size(); i++)
-        v[i]=v[i]+v[i-1];
     sort(v.begin(),v.end());
-    cout<<v[v.size()-1];
+    c=1;
+    ll e=v[0].first;
+    for(i=1;i<n; i++)
+    {
+        if(e<=v[i].second)
+        {
+            e=v[i].first;
+            c++;
+        }
+    }
+    cout<<c;
+
 
 return 0;
 }
