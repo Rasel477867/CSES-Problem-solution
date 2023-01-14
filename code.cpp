0dc439ff40da2a265bd1e06306e42bd998401e11
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-01-14 08:02:36 +0600

    CSES-Apartment-problem solution
    https://cses.fi/problemset/result/5281866/

--- code.cpp
+++ code.cpp
@@ -1,39 +1,54 @@
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 #define endl '\n'
 #define ll long long int
 #define pb push_back
-#define ps push
-#define pp pop
-vector<pair<ll,ll> >v;
-
-
-
+map<ll,ll>m;
+map<ll,ll>:: iterator it;
+vector<ll>v;
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll n,a,b,c,i;
-    cin>>n;
-    for(i=0; i<n; i++)
-    {
-        cin>>a>>b;
-        v.pb({b,a});
-    }
-    sort(v.begin(),v.end());
-    c=1;
-    ll e=v[0].first;
-    for(i=1;i<n; i++)
-    {
-        if(e<=v[i].second)
-        {
-            e=v[i].first;
-            c++;
-        }
-    }
-    cout<<c;
+     ll n,m1,i,a,k,b,ans=0;
+     cin>>n>>m1>>k;
+     for(i=0; i<n; i++)
+     {
+         cin>>a;
+         v.pb(a);
+     }
+     for(i=0; i<m1; i++)
+     {
+         cin>>a;
+        m[a]= m[a]+1;
+     }
+     sort(v.begin(),v.end());
+
+     for(i=0; i<n; i++)
+     {
+         a=v[i]-k;
+         b=v[i]+k;
+         it=m.lower_bound(a);
+
+         if((it->second>0 && it->first<=b) && it!=m.end())
+         {
+             ans++;
+             m[it->first]-=1;
+             if(it->second==0)
+                m.erase(it);
+         }
 
 
-return 0;
+     }
+
+
+  cout<<ans;
+
+
+    return 0;
 }
