d97e6755c5d1037db0e5b28a189bf044e2f54f5a
Rasel477867 <113754632+Rasel477867@users.noreply.github.com>
2023-08-05 12:35:16 +0600

    Static Range Sum Queries-solution
    https://cses.fi/problemset/result/6693396/

--- code.cpp
+++ code.cpp
@@ -1,3 +1,4 @@
+
 #include <ext/pb_ds/assoc_container.hpp> // Common file
 #include <ext/pb_ds/tree_policy.hpp>
 #include <functional> // for less
@@ -6,24 +7,20 @@
 #define endl '\n'
 #define ll long long int
 #define pb push_back
+#define pf push_front
 
-
-using namespace __gnu_pbds;
-using namespace std;
-
-typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag,
-        tree_order_statistics_node_update>
-        ordered_multiset;
-//ordered_multiset s3;
-//ordered_multiset :: iterator it;
 const ll mod=1e9+7;
-ll comb[55][55];
-const ll N=1e7;
-bool sive[N];
-vector<ll>prime;
+using namespace std;
 vector<ll>v;
-ll fact[N];
-
+//ll ex(ll a,ll b,ll mod)
+//{
+//    if(b==0)
+//        return 1;
+//    else if(b%2==0)
+//        return (ex((a*a)%mod,b/2,mod));
+//    else
+//        return (a*ex((a*a)%mod,(b-1)/2,mod))%mod;
+//}
 //long long int gcd(long long int a,long long int b)
 //{
 //    long long int c;
@@ -36,85 +33,30 @@ ll fact[N];
 //    return b;
 //}
 
-ll ex(ll a,ll b,ll mod)
-{
-    if(b==0)
-        return 1;
-    else if(b%2==0)
-        return (ex((a*a)%mod,b/2,mod));
-    else
-        return (a*ex((a*a)%mod,(b-1)/2,mod))%mod;
-}
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    ll i,j;
-//    for(i=0; i<N; i++)
-//    {
-//        sive[i]=true;
-//    }
-//    sive[0]=false;
-//    sive[1]=false;
-//    for(i=2; i*i<=N; i++)
-//    {
-//        if(sive[i])
-//        {
-//            for(j=i*i; j<N; j+=i)
-//            {
-//                sive[j]=false;
-//            }
-//        }
-//    }
-//    for(i=0; i<N; i++)
-//    {
-//        if(sive[i]==true)
-//            prime.pb(i);
-//    }
-//    for(i=0; i<=50; i++)
-//    {
-//        for(j=0; j<=i; j++)
-//        {
-//            if(i==j)
-//                comb[i][j]=1;
-//            else if(j==0)
-//                comb[i][j]=1;
-//            else
-//                comb[i][j]=comb[i-1][j-1]+comb[i-1][j];
-//        }
-//    }
-//
- fact[0]=1;
- fact[1]=1;
- for(i=1;i<N; i++)
- {
-     fact[i]=(((fact[i-1])%mod)*(i%mod))%mod;
- }
-
-    //start main funciton
-   ll n,a,b,c,ans=0;
-   cin>>n;
-   a=fact[n];
-   for(i=2; i<=n; i++)
-   {
-       if(i%2==0)
-       {
-           b=fact[i];
-           b=ex(b,mod-2,mod);
-           c=(a*b)%mod;
-           ans=(ans%mod+c%mod)%mod;
-       }
-       else
-       {
-           b=fact[i];
-           b=ex(b,mod-2,mod);
-           c=(a*b)%mod;
-           ans=(ans%mod-c%mod+mod)%mod;
-       }
-   }
-   cout<<ans;
-
+    ll i,j,t;
+    ll ans,sum,a,b,n;
+    cin>>n>>t;
+    cin>>a;
+    sum=a;
+    v.pb(0);
+    v.pb(a);
+    for(i=1; i<n; i++)
+    {
+        cin>>a;
+        sum+=a;
+        v.pb(sum);
+    }
+    while(t--)
+    {
+        cin>>a>>b;
+        ans=v[b]-v[a-1];
+        cout<<ans<<endl;
+    }
 
 
     return 0;
